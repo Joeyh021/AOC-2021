bbdd22be9d73199d4f36c475f61131da4c5a00b5
Joe Harrison <josephharrison1@me.com>
2021-12-04 01:54:55 +0000

    changed things to txts

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -4,9 +4,9 @@ mod day3;
 
 pub fn solve_day(day: u32) {
     let answer: (String, String) = match day {
-        1 => day1::solution("input/01"),
-        2 => day2::solution("input/02"),
-        3 => day3::solution("input/03"),
+        1 => day1::solution("input/01.txt"),
+        2 => day2::solution("input/02.txt"),
+        3 => day3::solution("input/03.txt"),
         _ => panic!("Not done this day yet"),
     };
     println!("Day {} solution:", day);
