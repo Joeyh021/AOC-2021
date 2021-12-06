fdd8ab02e6bc933385412f8cf8fa055989e0e66a
Joey <josephharrison1@me.com>
2021-12-06 15:21:51 +0000

    part 1 done

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -1,19 +1,25 @@
 use crate::Answer;
 
 pub fn solution(input: String) -> Answer<usize, usize> {
-    let day0: Vec<u32> = input.split(',').map(|s| s.parse::<u64>().unwrap).collect();
-    for i in 0..80 {
-        breed(day0);
+    let mut day0: Vec<u64> = input
+        .split(',')
+        .map(|s| s.parse::<u64>().unwrap())
+        .collect();
+    for _ in 0..256 {
+        breed(&mut day0);
     }
+    Answer(day0.len(), 0usize)
 }
 
-fn breed(fish: &mut Vec<u64>) -> Vec<u64> {
-    for f in fish {
-        if f == 0 {
-            f = 6;
-            next.push(8);
+fn breed(fish: &mut Vec<u64>) {
+    let mut new = 0;
+    for f in fish.iter_mut() {
+        if *f == 0u64 {
+            *f = 6u64;
+            new += 1;
         } else {
-            f = f - 1;
+            *f -= 1u64;
         }
     }
+    fish.append(&mut vec![8; new]);
 }
